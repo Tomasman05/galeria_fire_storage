bc506ffd0923d14255afc0e38f12b2637ecea987
Sulan Ádám <sulan.adam19@gmail.com>
2023-11-27 11:00:32 +0100

    updated, faulty

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
 
-  constructor() { }
+  constructor(private storage: AngularFireStorage, private db: AngularFireDatabase) {
+
+  }
+  uploadFile(file: any) {
+    const filename = "/feltolt " + file.name
+    const storageRef = this.storage.ref(filename)
+    const updateTask = this.storage.upload(filename,file)
+
+    this.updateTask.snapshotChanges().subscribe(
+      {
+        next: (ref: any) => {
+          console.log(ref)
+        },
+        error: (err: any) => console.log(err),
+        complete: () =>
+          storageRef.getDownloadURL().subscribe(
+            (url) => {
+              this.saveFileData({ name: file.name, url: file.url })
+              console.log(url)
+            }
+          )
+      }
+    )
+    return updateTask.percentageChanges()
+  }
+  saveFileData(fileData: any) {
+    this.db.list("/feltolt/").push(fileData)
+  }
 }
--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BaseService } from '../base.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload-file.component.css']
 })
 export class UploadFileComponent {
+  constructor(private base: BaseService) { }
+  selectedFile: any
+  percentage: any
 
+  selectFile(event: any) {
+    this.selectedFile = event.target.files[0]
+    this.percentage=0
+  }
+  uploadFile() {
+    this.base.uploadFile(this.selectedFile).subscribe(
+      (p) => this.percentage = Math.floor(Number (p)))
+  }
 }
+
