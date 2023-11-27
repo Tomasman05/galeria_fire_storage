d967234d6016ff8dcebeed75fcc62082be40c1dc
Sulan Ádám <sulan.adam19@gmail.com>
2023-11-27 09:02:12 +0100

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UploadFileComponent } from './upload-file/upload-file.component';
+import { FileListComponent } from './file-list/file-list.component';
+import { Environments } from './envinroments';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFileComponent,
+    FileListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    AngularFireModule.initializeApp(Environments.firebaseConfig),
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/envinroments.ts
@@ -0,0 +1,13 @@
+export const Environments = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBOr3N0dix5egxslLmQFrlkXFpsZK73w2U",
+        authDomain: "galeria-36c00.firebaseapp.com",
+        databaseURL: "https://galeria-36c00-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "galeria-36c00",
+        storageBucket: "galeria-36c00.appspot.com",
+        messagingSenderId: "1093155420320",
+        appId: "1:1093155420320:web:05292216d7c27b1b1ef1c5",
+        measurementId: "G-1R6382VBM8"
+    }
+}
